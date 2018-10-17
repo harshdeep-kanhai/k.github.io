e967b45982d0483dd8d4be56a07ed3718497cc2b
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2018-10-17 18:14:38 +0530

    Updates

---
+++ precache-manifest.7a49b2284266ee256600de43b6f9262b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "266769f57ea434886d3c",
+    "url": "/static/js/main.266769f5.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "266769f57ea434886d3c",
+    "url": "/static/css/main.22532c29.chunk.css"
+  },
+  {
+    "revision": "61d9197ab0a1403548e0fe139f5cfa55",
+    "url": "/index.html"
+  }
+];
